d4424b9171470c43385f3325178c19cabe07e526
yatakemi <thegraphiters01@gmail.com>
2018-08-28 01:01:11 +0900

    add version file

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ import (
 	"os"
 )
 
-const Name = "gommy"
-const Usage = "The command create a dummy data file based on a definition file."
-const Version = "0.0.1"
-
 func main() {
 	cli := &CLI{outStream: os.Stdout, errStream: os.Stderr}
 	os.Exit(cli.Run(os.Args))
---
+++ version.go
@@ -0,0 +1,7 @@
+package main
+
+const (
+	Name    = "gommy"
+	Usage   = "The command create a dummy data file based on a definition file."
+	Version = "0.0.1"
+)
